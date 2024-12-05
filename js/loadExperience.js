fd25a2457ca2f0c4aea94d6b857ae9e363d5fcdd
Valderrama20 <jose23122009@gmail.com>
2024-12-05 14:33:07 +0100

    acomodando informacion

---
+++ js/loadExperience.js
@@ -0,0 +1,61 @@
+const experiences = [
+  {
+    role: "Full-stack Developer",
+    company: "TPEOficial",
+    period: "Ago. 2024 / Actualidad",
+    description:
+      "Desarrollo de aplicaciones completas, trabajando en el diseño, implementación y mantenimiento de sistemas. Innovación en funcionalidades y resolución de problemas.",
+  },
+  {
+    role: "Full-stack Developer",
+    company: "Freelancer",
+    period: "Feb. 2024 / Actualidad",
+    description:
+      "Proyectos personalizados para clientes, abarcando desde el desarrollo de aplicaciones web hasta la integración de tecnologías modernas según las necesidades específicas.",
+  },
+  {
+    role: "Front-end Developer",
+    company: "Neefter",
+    period: "Feb. 2023 / Mar. 2023",
+    description:
+      "Especialización en interfaces de usuario. Implementación de diseños atractivos y funcionales, garantizando una experiencia fluida.",
+  },
+  {
+    role: "Full-stack Developer",
+    company: "Henry",
+    period: "Ago. 2022 / Sept. 2022",
+    description:
+      "Participación en proyectos web integrales, abordando tanto el front-end como el back-end, con un enfoque en la creación de soluciones escalables y de alto rendimiento, garantizando estándares de calidad en el desarrollo.",
+  },
+];
+
+const createCard = ({ role, company, period, description }, index) => {
+  const card = document.createElement("div");
+  card.classList.add("experience-card", "fade-in");
+  card.style.animationDelay = `${index / 10}s`;
+  card.innerHTML = `
+                <div class="line">
+                </div>
+                <div class="info-card">
+                    <h2>
+                        ${role}
+                    </h2>
+                    <p>${company}</p>
+                    <span>${period}</span>
+                </div>
+                <div class="description-card">
+                    <p>
+                    ${description}
+                    </p>
+                    <button>Saber mas <i class="fa-solid fa-angle-right"></i></button>
+                </div>
+            `;
+  return card;
+};
+
+(() => {
+  const cardContainer = document.getElementById("cardContainer");
+  experiences.forEach((proyect, index) =>
+    cardContainer.appendChild(createCard(proyect, index))
+  );
+})();
--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const proyects = [
   {
+    img: "img/proyecto5.png",
+    title: "Job Match",
+    description: `JobMatch es una innovadora aplicación creada para resolver la dificultad común de encontrar profesionales 
+    calificados para tareas del hogar. Nuestra plataforma facilita a los usuarios la búsqueda de profesionales confiables y 
+    altamente capacitados para una amplia gama de servicios, desde reparaciones de electrodomésticos hasta remodelaciones. 
+    Además, brinda a los profesionales la oportunidad de expandir su alcance, ofreciendo sus servicios a una base de clientes
+    más amplia y mejorando su visibilidad y posibilidades de trabajo.`,
+  },
+  {
     img: "img/proyecto1.png",
     title: "Prueba de titulo",
     description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ipsum veritatis maiores
@@ -15,10 +24,12 @@ const proyects = [
   },
   {
     img: "img/proyecto3.png",
-    title: "Prueba de titulo",
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga ipsum veritatis maiores
-                        praesentium eligendi adipisci consectetur libero, rerum odio iste perferendis, corrupti est
-                        facere exercitationem eum suscipit totam! Fugiat, maxime?`,
+    title: "Look House",
+    description: `Colaboré en un equipo de alto rendimiento en el desarrollo de una plataforma web, utilizando 
+      sprints semanales para avanzar de manera eficiente. A lo largo del proceso, presentamos progresos constantes
+      al Product Owner, garantizando una comunicación efectiva y alineación con los objetivos. Aplicamos diversas 
+      tecnologías y lenguajes aprendidos en el bootcamp de Henry, con el objetivo de mejorar la experiencia de los
+      usuarios al momento de alquilar una vivienda, simplificando cada paso del proceso.`,
   },
   {
     img: "img/proyecto4.png",
